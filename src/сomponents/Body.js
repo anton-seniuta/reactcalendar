772c0cd82c561ae1238243746e55786419105a0e
Seniuta Anton <senita.anton@gmail.com>
2022-08-22 22:47:09 +0300

    feat: right month showing

--- src/сomponents/Body.js
+++ src/сomponents/Body.js
@@ -1,55 +1,65 @@
+// import { useState } from "react";
 import "./Body.css";
 import Calendar from "./Calendar";
 
-function Body() {
-
+function Body() { 
   let todaysDate = new Date();
-  let currentDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), 1);
-  let date = {
-    year: currentDate.getFullYear(),
-    month: currentDate.getMonth(),
-  };
-  let firstCalendarDate = createFirstCalendarDate(date.year, date.month);
+  // let currentDate = Date.now(todaysDate.getFullYear(), todaysDate.getMonth(), 1);
+  // let currentDateForCount = new Date(currentDate);
+  let firstCalendarDate = createFirstCalendarDate(todaysDate.getFullYear(), todaysDate.getMonth());
+
   let calendarDays = calendarDaysCalculating(firstCalendarDate);
-  console.log(calendarDays)
+  // const [calendarDays, setCalendarDays] = useState(calendarDaysCalculating(firstCalendarDate));
 
   return (
     <div className="body">
       <div className="menu">
-        <button className="previous-button menu-button" onClick={()=> previousButtonClick(currentDate,calendarDays,firstCalendarDate)}>&lt;</button>
-        <h2 className="month-text">{textOutput(date.month, date.year)}</h2>
-        <button className="next-button menu-button" onClick={()=> nextButtonClick(currentDate)}>&gt;</button>
-        <button className="today-button menu-button" onClick={()=> todaysButtonClick(currentDate, todaysDate)}> Сегодня </button>
+        <button className="previous-button menu-button" onClick={() => previousButtonClick()}>
+          &lt;
+        </button>
+        <h2 className="month-text">{textOutput(todaysDate.getFullYear(), todaysDate.getMonth())}</h2>
+        <button className="next-button menu-button" onClick={() => nextButtonClick()}>
+          &gt;
+        </button>
+        <button className="today-button menu-button" onClick={() => todaysButtonClick()}>
+          Сегодня
+        </button>
       </div>
-      {/* <Calendar firstDay={firstCalendarDate}></Calendar>  */}
-      <Calendar calendarDays={calendarDays}></Calendar> 
-
+      <Calendar calendarDays={calendarDays}></Calendar>
     </div>
   );
 }
 
 export default Body;
 
-function calendarDaysCalculating(dayDate){
-
-  let month = dayDate.getMonth(); 
+function calendarDaysCalculating(firstDayDate) {
+  let date = new Date(firstDayDate);
+  let month = date.getMonth();
   let calendarDays = [];
+
+  // let calendarDays= {
+  //    id:
+  // };
+  console.log(month);
+  console.log(date.getMonth());
+
   for (;;) {
-    for (let i = 0; i < 7; i++) { 
-      calendarDays.push(dayDate.getDate() + "");
-      dayDate.setDate(dayDate.getDate() + 1);
+    for (let i = 0; i < 7; i++) {
+      calendarDays.push(date.getDate() + "");
+      date.setDate(date.getDate() + 1);
     }
-    if (dayDate.getMonth() !== month + 1) {
-      calendarDays[0] = 'Понедельник, ' + calendarDays[0];
-      calendarDays[1] = 'Вторник, ' + calendarDays[1];
-      calendarDays[2] = 'Среда, ' + calendarDays[2];
-      calendarDays[3] = 'Четверг,  ' + calendarDays[3]; 
-      calendarDays[4] = 'Пятница, ' + calendarDays[4];
-      calendarDays[5] = 'Суббота, ' + calendarDays[5];
-      calendarDays[6] = 'Воскресенье, ' + calendarDays[6];
+    if (date.getMonth() === month + 1) {
+      calendarDays[0] = "Понедельник, " + calendarDays[0];
+      calendarDays[1] = "Вторник, " + calendarDays[1];
+      calendarDays[2] = "Среда, " + calendarDays[2];
+      calendarDays[3] = "Четверг,  " + calendarDays[3];
+      calendarDays[4] = "Пятница, " + calendarDays[4];
+      calendarDays[5] = "Суббота, " + calendarDays[5];
+      calendarDays[6] = "Воскресенье, " + calendarDays[6];
       break;
     }
   }
+  console.log(calendarDays);
   return calendarDays;
 }
 
@@ -60,59 +70,57 @@ function createFirstCalendarDate(year, month) {
   } else {
     result.setDate(result.getDate() - result.getDay() + 1);
   }
-  return result;
+  return result.getTime();
 }
 
-function textOutput(month, year){
-  let output ="";
-  switch(month.toString()){
-    case '0': output += "Январь";  
-    break;
-    case '1': output += "Февраль";  
-    break;
-    case '2': output += "Март";  
-    break;
-    case '3': output += "Апрель";  
-    break;
-    case '4': output += "Май";  
-    break;
-    case '5': output += "Июнь";  
-    break;
-    case '6': output += "Июль";  
-    break;
-    case '7': output += "Август";  
-    break;
-    case '8': output += "Сентябрь";  
-    break;
-    case '9': output += "Октябрь";  
-    break;
-    case '10': output += "Ноябрь"; ;
-    break;
-    case '11': output += "Декабрь"; ;
-    break;
-    default: console.log("something is wrong in switch");
-    break;
+function textOutput(year, month) {
+  let output = "";
+  switch (month.toString()) {
+    case "0":
+      output += "Январь";
+      break;
+    case "1":
+      output += "Февраль";
+      break;
+    case "2":
+      output += "Март";
+      break;
+    case "3":
+      output += "Апрель";
+      break;
+    case "4":
+      output += "Май";
+      break;
+    case "5":
+      output += "Июнь";
+      break;
+    case "6":
+      output += "Июль";
+      break;
+    case "7":
+      output += "Август";
+      break;
+    case "8":
+      output += "Сентябрь";
+      break;
+    case "9":
+      output += "Октябрь";
+      break;
+    case "10":
+      output += "Ноябрь";
+      break;
+    case "11":
+      output += "Декабрь";
+      break;
+    default:
+      console.log("something is wrong in switch");
+      break;
   }
-  return output + ' ' + year;
+  return output + " " + year;
 }
 
-function previousButtonClick(date, calendarDays, firstCalendarDate){
-  console.log(date);
-  date.setMonth(date.getMonth() - 1);
-  firstCalendarDate = createFirstCalendarDate(date.year, date.month);
-  calendarDays = calendarDaysCalculating(firstCalendarDate);
-  console.log(date);
-  console.log("calendar: ", calendarDays);
-}
+function previousButtonClick() {}
 
-function nextButtonClick(date){
-  console.log(date);
-  date.setMonth(date.getMonth() + 1);
-  console.log(date);
-}
+function nextButtonClick() {}
 
-function todaysButtonClick(date ,todaysDate){
-  console.log(date);
-  date.setMonth(todaysDate.getMonth());
-  console.log(date);
-}
+function todaysButtonClick() {}
--- src/сomponents/Calendar.js
+++ src/сomponents/Calendar.js
@@ -1,13 +1,9 @@
+import React from "react";
 import "./Calendar.css";
 import Day from "./Day";
 
 
-function Calendar(props) {
-
-  // let dayDate = new Date(props.firstDay.getFullYear(), props.firstDay.getMonth(), props.firstDay.getDate());
-  // let calendarDays = calendarDaysCalculating(dayDate); //make  objects  add ids  
-  let calendarDays = props.calendarDays;
-  
+function Calendar({calendarDays}) {
   return (
     <div className="calendar">
       {calendarDays.map((text, index) => (
@@ -16,36 +12,27 @@ function Calendar(props) {
       }
     </div>
   );
-
 }
-  
 export default Calendar;
- 
-// function calendarDaysCalculating(dayDate){
 
-//   let month = dayDate.getMonth(); 
-//   let calendarDays = [];
-//   for (;;) {
-//     for (let i = 0; i < 7; i++) { 
-//       calendarDays.push(dayDate.getDate() + "");
-//       dayDate.setDate(dayDate.getDate() + 1);
+// class Calendar extends React.Component{
+//   constructor(props) {
+//     super(props); 
+//     this.state = {
+//       calendarDays: props.calendarDays,
 //     }
-//     if (dayDate.getMonth() !== month + 1) {
-//       calendarDays[0] = 'Понедельник, ' + calendarDays[0];
-//       calendarDays[1] = 'Вторник, ' + calendarDays[1];
-//       calendarDays[2] = 'Среда, ' + calendarDays[2];
-//       calendarDays[3] = 'Четверг,  ' + calendarDays[3]; 
-//       calendarDays[4] = 'Пятница, ' + calendarDays[4];
-//       calendarDays[5] = 'Суббота, ' + calendarDays[5];
-//       calendarDays[6] = 'Воскресенье, ' + calendarDays[6];
-//       break;
-//     }
+    
 //   }
-//   console.log(calendarDays)
-//   return calendarDays;
+//   render(){
+//     return(
+//     <div className="calendar">
+//       {this.props.calendarDays.map((text, index) => (
+//       <Day key={index} day={text}/>
+//       ))
+//       }
+//     </div>
+//     )
+//   }
 // }
 
-//buttons
-//inputting notes
-//highlighting notes
-//local storage
+// export default Calendar;
